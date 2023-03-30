6ec7a5e463cd59aec34e25c2825d78db5ef3b2f3
kenny-dinh-codeleap <kenny.dinh@codeleap.de>
2023-03-30 10:40:39 +0700

    update core libs

--- lib/hyperledger-farbric-multi-region-stack.ts
+++ lib/hyperledger-farbric-multi-region-stack.ts
@@ -7,6 +7,9 @@ import * as ec2peer from "aws-cdk-lib/aws-ec2-peer";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as route53targets from "aws-cdk-lib/aws-route53-targets";
 import { Construct } from "constructs/lib/construct";
+import { DefaultCapacityType } from "aws-cdk-lib/aws-eks";
+import { HyperledgerFabricNetwork } from "@cdklabs/cdk-hyperledger-fabric-network";
+import * as hyperledger from "@cdklabs/cdk-hyperledger-fabric-network";
 
 export class HyperLedgerFarbricMultiRegionStack extends cdk.Stack {
   constructor(app: Construct, id: string, props?: cdk.StackProps) {
@@ -60,6 +63,72 @@ export class HyperLedgerFarbricMultiRegionStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
           name: "Private",
         },
+      ],
+    });
+
+    // Define the EKS cluster in us-east-1 region
+    const clusterUsEast1 = new eks.Cluster(
+      app,
+      "Hyperledger-Fabric-Cluster-UsEast1",
+      {
+        vpc: vpcUsEast1,
+        defaultCapacity: 2,
+        clusterName: "hyperledger-fabric-cluster-us-east-1",
+        version: eks.KubernetesVersion.V1_25,
+      }
+    );
+
+    // Define the EKS cluster in us-west-2 region
+    const clusterUsWest2 = new eks.Cluster(
+      app,
+      "Hyperledger-Fabric-Cluster-UsWest2",
+      {
+        vpc: vpcUsWest2,
+        defaultCapacity: 2,
+        defaultCapacityType: DefaultCapacityType.EC2,
+        clusterName: "hyperledger-fabric-cluster-us-west-2",
+        version: eks.KubernetesVersion.V1_25,
+        region: "us-west-2",
+      }
+    );
+
+    // Define the EKS cluster in eu-west-1 region
+    const clusterEuWest1 = new eks.Cluster(
+      app,
+      "Hyperledger-Fabric-Cluster-EuWest1",
+      {
+        vpc: vpcEuWest1,
+        defaultCapacity: 2,
+        clusterName: "hyperledger-fabric-cluster-eu-west-1",
+        version: eks.KubernetesVersion.V1_25,
+        region: "eu-west-1",
+      }
+    );
+
+    new HyperledgerFabricNetwork(this, "Example", {
+      networkName: "MyNetwork",
+      networkDescription: "This is my Hyperledger Fabric network",
+      memberName: "MyMember",
+      frameworkVersion: hyperledger.FrameworkVersion.VERSION_2_2,
+      proposalDurationInHours: 48,
+      thresholdPercentage: 75,
+      nodes: [
+        {
+          availabilityZone: "us-east-1a",
+          instanceType: hyperledger.InstanceType.STANDARD5_LARGE,
+        },
+        {
+          availabilityZone: "us-east-1b",
+          instanceType: hyperledger.InstanceType.STANDARD5_LARGE,
+        },
+        {
+          availabilityZone: "",
+          instanceType: hyperledger.InstanceType.BURSTABLE3_SMALL,
+        },
+      ],
+      users: [
+        { userId: "AppUser1", affilitation: "MyMember" },
+        { userId: "AppUser2", affilitation: "MyMember.department1" },
       ],
     });
   }
